cdca3072556c10edb620a17cba9ceb79eaa4632a
Will Hodgson <will.hodgson@current-rms.com>
2019-08-21 09:37:56 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,6 @@
 class Person
 
   def initialize(attributes)
-    @name = attributes.name
-    @birthday = attributes.birthday
-    @hair_color = attributes.hair_color
+    attributes.each {|key, value| self.send()}
   end
 end
